bb81a0f4f22ee254294a0f18e20c6ff3116c11d7
Guille Gonzalez <gonzalezreal@icloud.com>
2017-08-13 11:55:23 +0200

    Add documentation

--- Sources/ErrorResponse.swift
+++ Sources/ErrorResponse.swift
@@ -1,10 +1,18 @@
 import Foundation
 
+/// An error response.
 public struct ErrorResponse: Decodable {
+
+	/// Defines an error.
 	public struct Error: Decodable {
+
+		/// Error code.
 		public let code: String
+
+		/// A description of the error.
 		public let message: String
 	}
 
+	/// An Error object that contains information about the error.
 	public let error: Error
 }
