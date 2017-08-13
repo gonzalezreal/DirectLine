0247b00e7b14a03031c7398ad7d30dea57944aab
Guille Gonzalez <gonzalezreal@icloud.com>
2017-08-13 12:15:03 +0200

    Add ActivitySet model

---
+++ Sources/ActivitySet.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/// Defines a set of activities.
+public struct ActivitySet: Decodable {
+
+	/// Array of Activity objects.
+	public let activities: [Activity]
+
+	/**
+		Maximum watermark of activities within the set.
+
+		A client may use the `watermark` value to indicate the most recent message
+		it has seen either when retrieving activities from the bot or when generating
+		a new WebSocket stream URL.
+	*/
+	public let watermark: String?
+}
---
+++ Tests/DirectLineTests/ActivitySetTest.swift
@@ -0,0 +1,59 @@
+import XCTest
+import DirectLine
+
+class ActivitySetTest: XCTestCase {
+	func testJSONProducesActivitySet() {
+		// given
+		let json = """
+		{
+			"activities": [
+				{
+					"type": "message",
+					"id": "GNqZzvYP9Zb2x4VoufTUzl|0000002",
+					"timestamp": "2017-08-13T09:32:28.5262964Z",
+					"channelId": "directline",
+					"from": {
+						"id": "user1"
+					},
+					"conversation": {
+						"id": "GNqZzvYP9Zb2x4VoufTUzl"
+					},
+					"text": "hello"
+				}
+			],
+			"watermark": "1"
+		}
+		""".data(using: .utf8)!
+		let decoder = JSONDecoder()
+
+		// then
+		XCTAssertNoThrow(try decoder.decode(ActivitySet.self, from: json))
+	}
+
+	func testJSONWithoutWatermarkProducesActivitySet() {
+		// given
+		let json = """
+		{
+			"activities": [
+				{
+					"type": "message",
+					"id": "GNqZzvYP9Zb2x4VoufTUzl|0000002",
+					"timestamp": "2017-08-13T09:32:28.5262964Z",
+					"channelId": "directline",
+					"from": {
+						"id": "user1"
+					},
+					"conversation": {
+						"id": "GNqZzvYP9Zb2x4VoufTUzl"
+					},
+					"text": "hello"
+				}
+			]
+		}
+		""".data(using: .utf8)!
+		let decoder = JSONDecoder()
+
+		// then
+		XCTAssertNoThrow(try decoder.decode(ActivitySet.self, from: json))
+	}
+}
