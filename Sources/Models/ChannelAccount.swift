19d0d0dd954b69792c3f65427073dc117088deb9
Guille Gonzalez <gonzalezreal@icloud.com>
2017-08-15 23:06:58 +0200

    Add ChannelAccount model

---
+++ Sources/Models/ChannelAccount.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+/// Defines a bot or user account on the channel.
+public struct ChannelAccount: Codable {
+
+	/// ID that uniquely identifies the bot or user on the channel.
+	public let id: String
+
+	/// Name of the bot or user.
+	public let name: String?
+}
