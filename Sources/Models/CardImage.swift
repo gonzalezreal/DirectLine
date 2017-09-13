4f3d57650ad14390a00b9c27bd50748a025ac201
Guille Gonzalez <gonzalezreal@icloud.com>
2017-09-13 14:37:12 +0200

    SUBO-714 Review feedback

--- Sources/Models/CardImage.swift
+++ Sources/Models/CardImage.swift
@@ -4,11 +4,17 @@ import Foundation
 public struct CardImage: Decodable {
 
 	/// Description of the image.
-	public let alt: String?
+	public let description: String?
 
 	/// A `CardAction` value that specifies the action to perform if the user taps or clicks the image.
 	public let tap: CardAction?
 
 	/// URL to the source of the image.
 	public let url: URL
+
+	private enum CodingKeys: String, CodingKey {
+		case description = "alt"
+		case tap
+		case url
+	}
 }
