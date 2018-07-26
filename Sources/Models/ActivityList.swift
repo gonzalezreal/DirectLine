6f24b27d1a1db71aed02c705b36ea22dc2aaa638
Guille Gonzalez <gonzalezreal@icloud.com>
2018-07-26 11:50:56 +0200

    Add ActivityList model

---
+++ Sources/Models/ActivityList.swift
@@ -0,0 +1,21 @@
+//
+// DirectLine
+//
+// Copyright © 2018 Guille Gonzalez. All rights reserved.
+// See LICENSE file for license.
+//
+
+import Foundation
+
+/// Defines a list of activities.
+struct ActivityList<ChannelData: Codable>: Decodable {
+	/// Array of `Activity` values.
+	let activities: [Activity<ChannelData>]
+
+	/// Maximum watermark of activities within the set.
+	///
+	/// A client may use the `watermark` value to indicate the most recent message
+	/// it has seen either when retrieving activities from the bot or when generating
+	/// a new WebSocket stream URL.
+	let watermark: String?
+}
