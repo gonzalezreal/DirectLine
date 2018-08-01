387e8e4ad47ed7159af91c43a4eb4fe270dae3b3
Guille Gonzalez <gonzalezreal@icloud.com>
2018-08-01 09:32:53 +0200

    Add empty sample project

---
+++ Example/ViewController.swift
@@ -0,0 +1,15 @@
+//
+// DirectLine
+//
+// Copyright © 2018 Guille Gonzalez. All rights reserved.
+// See LICENSE file for license.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+}
--- Tools/run-swiftformat.sh
+++ Tools/run-swiftformat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-"${PROJECT_DIR}/Tools/swiftformat" --indent tabs --stripunusedargs closure-only --decimalgrouping 3,4 --commas inline --disable hoistPatternLet --header "\nDirectLine\n\nCopyright © {year} Guille Gonzalez. All rights reserved.\nSee LICENSE file for license.\n" "${SRCROOT}/Sources/" "${SRCROOT}/Tests/"
+"${PROJECT_DIR}/Tools/swiftformat" --indent tabs --stripunusedargs closure-only --decimalgrouping 3,4 --commas inline --disable hoistPatternLet --header "\nDirectLine\n\nCopyright © {year} Guille Gonzalez. All rights reserved.\nSee LICENSE file for license.\n" "${SRCROOT}/Sources/" "${SRCROOT}/Tests/" "${SRCROOT}/Example/"
