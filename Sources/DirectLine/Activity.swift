ad139e5fa326952c69cbc84afd93c8c504250260
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-01-18 22:19:30 +0100

    Add activity type encoding & decoding

--- Sources/DirectLine/Activity.swift
+++ Sources/DirectLine/Activity.swift
@@ -1,4 +1,19 @@
 import Foundation
 
 /// An Activity is the basic communication type for the Bot Framework 3.0 protocol.
-public struct Activity<ChannelData>: Codable, Equatable where ChannelData: Codable, ChannelData: Equatable {}
+public struct Activity<ChannelData> {
+    /// Contains the activity type.
+    public let type: ActivityType
+}
+
+extension Activity: Codable where ChannelData: Codable {
+    public init(from decoder: Decoder) throws {
+        type = try ActivityType(from: decoder)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        try type.encode(to: encoder)
+    }
+}
+
+extension Activity: Equatable where ChannelData: Equatable {}
--- Sources/DirectLine/ActivityType.swift
+++ Sources/DirectLine/ActivityType.swift
@@ -7,14 +7,41 @@ public enum ActivityType: Equatable {
     case unknown
 }
 
-extension ActivityType: Decodable {
-    public init(from _: Decoder) throws {
-        fatalError()
+extension ActivityType: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case type
     }
-}
 
-extension ActivityType: Encodable {
-    public func encode(to _: Encoder) throws {
-        fatalError()
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(String.self, forKey: .type)
+
+        switch type {
+        case "message":
+            self = try .message(MessageActivity(from: decoder))
+        case "endOfConversation":
+            self = try .endOfConversation(EndOfConversationActivity(from: decoder))
+        case "typing":
+            self = .typing
+        default:
+            self = .unknown
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        switch self {
+        case let .message(messageActivity):
+            try container.encode("message", forKey: .type)
+            try messageActivity.encode(to: encoder)
+        case let .endOfConversation(endOfConversationActivity):
+            try container.encode("endOfConversation", forKey: .type)
+            try endOfConversationActivity.encode(to: encoder)
+        case .typing:
+            try container.encode("typing", forKey: .type)
+        case .unknown:
+            fatalError("Invalid activity type.")
+        }
     }
 }
