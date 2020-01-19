1492c474c98b9f7c3eb82cd688adb4ef078acbe1
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-01-19 11:32:05 +0100

    Implement activity properties

---
+++ Sources/DirectLine/ConversationAccount.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+/// Defines a conversation in a channel.
+public struct ConversationAccount: Codable, Equatable {
+    /// The ID that identifies the conversation.
+    public let id: String
+
+    /// Friendly name for the conversation within the channel.
+    public let name: String?
+
+    /// This account's object ID within Azure Active Directory (AAD).
+    public let aadObjectId: String?
+
+    /// Indicates whether the conversation contains more than two participants at the time the activity was generated.
+    public let isGroup: Bool?
+
+    /// Indicates the type of the conversation in channels that distinguish between conversation types.
+    public let conversationType: String?
+
+    /// Role of the entity behind the account.
+    public let role: Role?
+}
