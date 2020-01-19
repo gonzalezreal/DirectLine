da179a297faf54b2e59761241e36279fb67a3962
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-01-19 11:57:27 +0100

    Add EndOfConversationActivity properties

--- Sources/DirectLine/EndOfConversationActivity.swift
+++ Sources/DirectLine/EndOfConversationActivity.swift
@@ -1,3 +1,10 @@
 import Foundation
 
-public struct EndOfConversationActivity: Codable, Equatable {}
+/// End of conversation activities signal the end of a conversation from the recipient's perspective.
+public struct EndOfConversationActivity: Codable, Equatable {
+    /// Optional text content to be communicated to a user.
+    public let text: String?
+
+    /// Describes why or how the conversation was ended.
+    public let code: String?
+}
