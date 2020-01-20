55c7cb808cfd68394bb839856bc25baef4e14c53
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-01-20 22:02:12 +0100

    Add some documentation

--- Sources/DirectLine/CodableAdditions.swift
+++ Sources/DirectLine/CodableAdditions.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 internal extension KeyedDecodingContainer {
+    // Decode null or not specified array into an empty array
     func decode<A>(_: A.Type, forKey key: Key) throws -> A where A: Decodable, A: RangeReplaceableCollection {
         if let value = try decodeIfPresent(A.self, forKey: key) {
             return value
@@ -11,6 +12,7 @@ internal extension KeyedDecodingContainer {
 }
 
 internal extension KeyedEncodingContainer {
+    // Do not encode array if empty
     mutating func encode<A>(_ value: A, forKey key: Key) throws where A: Encodable, A: Collection {
         guard !value.isEmpty else { return }
         try encode(value, forKey: key)
