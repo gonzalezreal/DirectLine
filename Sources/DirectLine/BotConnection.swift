a248ba7ff308464d06696080430eb96ce7585ea3
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-01-24 15:30:01 +0100

    [WIP] Implement BotConnection

--- Sources/DirectLine/BotConnection.swift
+++ Sources/DirectLine/BotConnection.swift
@@ -3,7 +3,7 @@ import Foundation
 import Logging
 import SimpleNetworking
 
-public class BotConnection<ChannelData> {
+public class BotConnection<ChannelData> where ChannelData: Codable {
     public var state: AnyPublisher<BotConnectionState, Never> {
         stateSubject.eraseToAnyPublisher()
     }
@@ -12,22 +12,68 @@ public class BotConnection<ChannelData> {
         activitiesSubject.eraseToAnyPublisher()
     }
 
-    private let apiClient = APIClient(baseURL: .directLine)
+    private let auth: Auth
+    private let apiClient: APIClient
     private let stateSubject = CurrentValueSubject<BotConnectionState, Never>(.uninitialized)
     private let activitiesSubject = PassthroughSubject<Activity<ChannelData>, Never>()
-    
-    private let secret: String
+    private let syncQueue = DispatchQueue(label: "com.gonzalezreal.DirectLine.BotConnection")
     private var logger = Logger(label: "BotConnection")
 
-    public init(secret: String, logLevel: Logger.Level) {
-        self.secret = secret
-        
-        // TODO: add the logLevel as a construction parameter in APIClient
-        apiClient.logger.logLevel = logLevel
+    public init(_ auth: Auth, logLevel: Logger.Level) {
+        self.auth = auth
+        apiClient = APIClient(baseURL: .directLine, logLevel: logLevel)
         logger.logLevel = logLevel
     }
 
-    public func postActivity(_: Activity<ChannelData>) -> AnyPublisher<ResourceResponse, Error> {
-        fatalError("TODO: implement")
+    /// Send an activity to the bot.
+    /// - Parameter activity: Activity to send.
+    public func postActivity(_ activity: Activity<ChannelData>) -> AnyPublisher<ResourceResponse, BotConnectionError> {
+        // Get the current conversation or start one if necessary
+        conversation().first()
+            .flatMap { [apiClient] conversation -> AnyPublisher<ResourceResponse, BotConnectionError> in
+                // Post the activity in the conversation
+                apiClient.response(for: .postActivity(token: conversation.token, conversationId: conversation.conversationId, activity: activity))
+                    .mapError { BotConnectionError(error: $0) }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+private extension BotConnection {
+    /// Return the current conversation or start one if necessary.
+    func conversation() -> AnyPublisher<Conversation, BotConnectionError> {
+        return stateSubject
+            .setFailureType(to: BotConnectionError.self)
+            .receive(on: syncQueue)
+            .flatMap { [weak self] connectionState -> AnyPublisher<BotConnectionState, BotConnectionError> in
+                // Start a new conversation if necessary
+                guard let self = self, case .uninitialized = connectionState else {
+                    return Just(connectionState)
+                        .setFailureType(to: BotConnectionError.self)
+                        .eraseToAnyPublisher()
+                }
+
+                // Let subscribers know that we are connecting
+                self.stateSubject.send(.connecting)
+
+                return self.startConversation()
+                    .map { BotConnectionState.ready($0) }
+                    // Let subscribers know that we are ready
+                    .handleEvents(receiveOutput: { self.stateSubject.send($0) })
+                    .eraseToAnyPublisher()
+            }
+            .filter { $0.isReadyOrHasFailed }
+            .tryMap { try $0.conversation() }
+            .mapError { BotConnectionError(error: $0) }
+            .eraseToAnyPublisher()
+    }
+
+    /// Start a new conversation.
+    func startConversation() -> AnyPublisher<Conversation, BotConnectionError> {
+        apiClient.response(for: .startConversation(auth))
+            .mapError { BotConnectionError(error: $0) }
+            .receive(on: syncQueue)
+            .eraseToAnyPublisher()
     }
 }
--- Sources/DirectLine/BotConnectionState.swift
+++ Sources/DirectLine/BotConnectionState.swift
@@ -1,10 +1,30 @@
 import Foundation
 
-public enum BotConnectionState {
+public enum BotConnectionState: Equatable {
     case uninitialized
     case connecting
-    case connected
-    case expiredToken
-    case failedToConnect
-    case closed
+    case ready(Conversation)
+    case failed(BotConnectionError)
+}
+
+internal extension BotConnectionState {
+    var isReadyOrHasFailed: Bool {
+        switch self {
+        case .ready, .failed:
+            return true
+        default:
+            return false
+        }
+    }
+
+    func conversation() throws -> Conversation {
+        switch self {
+        case let .ready(conversation):
+            return conversation
+        case let .failed(error):
+            throw error
+        default:
+            fatalError("Unexpected connection state: \(self)")
+        }
+    }
 }
